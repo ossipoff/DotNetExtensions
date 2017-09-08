36b62bb0ebf5e7bff69d68cb1c631aec47bec95b
ossipoff <ossipoff@gmail.com>
2017-09-08 22:03:03 +0200

    Implemented LatingDiacriticsToAscii and Slugify extension methods on string

--- DotNetExtensions.Test/StringTest.cs
+++ DotNetExtensions.Test/StringTest.cs
@@ -83,5 +83,59 @@ namespace DotNetExtensions.Test
 
             Assert.AreEqual("Lorem ipsu (...)", r);
         }
+
+        [TestMethod]
+        public void LatinDiacriticsToAscii_returns_string_where_latin_characters_are_replace_by_ascii_characters()
+        {
+            string s = "À à Á á Â â Ã ã Ä ä Å å Æ æ Ç ç È è É é Ê ê Ë ë Ì ì Í í Î î Ï ï ı Ñ ñ Ò ò Ó ó Ô ô Õ õ Ö ö Ø ø Œ œ Š š ẞ ß Ù ù Ú ú Û û Ü ü Ý ý Ÿ ÿ Ž ž Ð ð Þ þ";
+            string e = "A a A a A a A a Ae ae Aa aa Ae ae C c E e E e E e E e I i I i I i I i i N n O o O o O o O o Oe oe Oe oe Oe oe S s S s U u U u U u U u Y y Y y Z z Th th Eth eth";
+
+            var r = s.LatinDiacriticsToAscii();
+
+            Assert.AreEqual(e, r);
+        }
+
+        [TestMethod]
+        public void Slugify_correctly_turns_latin_string_with_spaces_and_upper_and_lowercase_characters_into_a_slug()
+        {
+            string s = "A a B b C c D d E e F f G g H h I i J j K k L l M m N n O o P p Q q R r S s T t U u V v W w X x Y y Z z";
+
+            var r = s.Slugify();
+
+            Assert.AreEqual("a-a-b-b-c-c-d-d-e-e-f-f-g-g-h-h-i-i-j-j-k-k-l-l-m-m-n-n-o-o-p-p-q-q-r-r-s-s-t-t-u-u-v-v-w-w-x-x-y-y-z-z", r);
+        }
+
+        [TestMethod]
+        public void Slugify_correctly_turns_latin_diacritic_string_with_spaces_and_upper_and_lowercase_characters_into_a_slug()
+        {
+            string s = "À à Á á Â â Ã ã Ä ä Å å Æ æ Ç ç È è É é Ê ê Ë ë Ì ì Í í Î î Ï ï ı Ñ ñ Ò ò Ó ó Ô ô Õ õ Ö ö Ø ø Œ œ Š š ẞ ß Ù ù Ú ú Û û Ü ü Ý ý Ÿ ÿ Ž ž Ð ð Þ þ";
+            string e = "a-a-a-a-a-a-a-a-ae-ae-aa-aa-ae-ae-c-c-e-e-e-e-e-e-e-e-i-i-i-i-i-i-i-i-i-n-n-o-o-o-o-o-o-o-o-oe-oe-oe-oe-oe-oe-s-s-s-s-u-u-u-u-u-u-u-u-y-y-y-y-z-z-th-th-eth-eth";
+
+            var r = s.Slugify();
+
+            Assert.AreEqual(e, r);
+        }
+
+        [TestMethod]
+        public void Slugify_correctly_turns_numeric_string_with_spaces_and_dots_into_a_slug()
+        {
+            string s = "0 1 2 3 4 5 6 7 8 9 1.0";
+            string e = "0-1-2-3-4-5-6-7-8-9-1.0";
+
+            var r = s.Slugify();
+
+            Assert.AreEqual(e, r);
+        }
+
+        [TestMethod]
+        public void Slugify_correctly_turns_non_alphanumeric_string_with_spaces_into_a_slug()
+        {
+            string s = "½ + ' < , . - § ! \" # ¤ % & / ( ) = * > ; : @ £ $ € { [ ] } | \\ µ";
+            string e = "-----.----------------------------";
+
+            var r = s.Slugify();
+
+            Assert.AreEqual(e, r);
+        }
     }
 }
--- DotNetExtensions/String.cs
+++ DotNetExtensions/String.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DotNetExtensions
@@ -28,6 +30,72 @@ namespace DotNetExtensions
         public static string Truncate(this string value, int maxLength, string omissionIndicator)
         {
             return (value?.Length).GetValueOrDefault() <= maxLength ? value : $"{value.Substring(0, maxLength)}{omissionIndicator}";
+        }
+
+        public static Dictionary<char, string> latinDiacriticsToAsciiReplacements = new Dictionary<char, string>()
+        {
+            { 'ı', "i" },
+            { 'ẞ', "S" },
+            { 'ß', "s" },
+            { 'Ð', "Th" },
+            { 'ð', "th" },
+            { 'Þ', "Eth" },
+            { 'þ', "eth" },
+            { 'Ä', "Ae" },
+            { 'ä', "ae" },
+            { 'Æ', "Ae" },
+            { 'æ', "ae" },
+            { 'Ö', "Oe" },
+            { 'ö', "oe" },
+            { 'Ø', "Oe" },
+            { 'ø', "oe" },
+            { 'Œ', "Oe" },
+            { 'œ', "oe" },
+            { 'Å', "Aa" },
+            { 'å', "aa" }
+        };
+
+        public static string LatinDiacriticsToAscii(this string text)
+        {
+            foreach (var replacement in latinDiacriticsToAsciiReplacements)
+            {
+                text = text.Replace(replacement.Key.ToString(), replacement.Value);
+            }
+
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+        private static readonly Regex slugifyRegex = new Regex(@"[^a-zA-Z0-9\-\.]");
+
+        private static Dictionary<string, string> slugifyReplacements = new Dictionary<string, string>()
+        {
+          { " ", "-" }
+        };
+
+        public static string Slugify(this string text)
+        {
+            text = text.ToLower();
+
+            foreach (var replacement in slugifyReplacements)
+            {
+                text = text.Replace(replacement.Key.ToString(), replacement.Value);
+            }
+
+            text = text.LatinDiacriticsToAscii().Replace(" ", "-");
+
+            return slugifyRegex.Replace(text, "");
         }
     }
 }
